c7062121ed3a57fe67052aa6828b273248f21e24
YuBeomGon <cleo8131@naver.com>
2022-05-30 10:59:06 +0900

    add sparse attn block for interface

--- cls_utils/block.py
+++ cls_utils/block.py
@@ -7,6 +7,8 @@ from torch.nn.modules.utils import _pair
 from torchvision.extension import _assert_has_ops
 from torchvision.ops.roi_pool import roi_pool
 from torchvision.models.resnet import conv1x1, conv3x3
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+
 
 
 class Bottleneck(nn.Module):
@@ -31,8 +33,8 @@ class Bottleneck(nn.Module):
         self.bn2 = norm_layer(width)
         self.conv3 = conv3x3(width, width, stride, groups, dilation)
         self.bn3 = norm_layer(width)
-        self.conv4 = conv1x1(width, planes * self.expansion)
-        self.bn4 = norm_layer(planes * self.expansion)        
+        self.conv4 = conv1x1(width, planes)
+        self.bn4 = norm_layer(planes)        
         self.relu = nn.ReLU(inplace=True)
         self.gelu = nn.GELU()
         self.downsample = downsample
@@ -67,9 +69,192 @@ class Bottleneck(nn.Module):
     
 # class SwinTransformerBlock(nn.Module):
 
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.conv1 = conv1x1(in_features, hidden_features)
+        self.act = act_layer()
+        self.conv2 = conv1x1(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
 
-# class SparseAttnBlock(nn.Module):
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.conv2(x)
+        x = self.drop(x)
+        return x
 
+def window_partition(x, window_size=8, islocal=True):
+    """
+    Args:
+        x: (B, C, H, W )
+        window_size (int): window size
+    Returns:
+        windows: (num_windows*B, window_size, window_size, C)
+    """
+    B, C, H, W = x.shape
+    x = x.view(B, C, H // window_size, window_size, W // window_size, window_size)
+    if islocal :
+        windows = x.permute(0, 2, 4, 3, 5, 1).contiguous().view(-1, window_size, window_size, C)
+        windows = windows.view(-1, window_size * window_size, C)
+    else :
+        windows = x.permute(0, 3, 5, 2, 4, 1).contiguous().view(-1, window_size, window_size, C)
+        windows = windows.view(-1, H//window_size * W//window_size, C)
+    return windows
+
+def window_reverse(windows, window_size, H, W, islocal=True):
+    """
+    Args:
+        windows: (num_windows*B, window_size, window_size, C)
+        window_size (int): Window sizewindow_size
+        H (int): Height of image
+        W (int): Width of image
+    Returns:
+        x: (B, C, H, W)
+    """
+    
+    if islocal :
+        B = int(windows.shape[0] / (H * W / window_size / window_size))
+        x = windows.view(B, -1, H // window_size, W // window_size, window_size, window_size)
+        x = x.permute(0, 1, 2, 4, 3, 5).contiguous().view(B, -1, H, W)
+    else :
+        B = int(windows.shape[0] / (window_size * window_size))
+        x = windows.view(B, -1, window_size, window_size, H // window_size, W // window_size)
+        x = x.permute(0, 1, 2, 4, 3, 5).contiguous().view(B, -1, H, W)        
+        
+    return x
+
+class WindowAttention(nn.Module):
+    r""" Window based multi-head self attention (W-MSA) module with relative position bias.
+    It supports both of shifted and non-shifted window.
+    Args:
+        dim (int): Number of input channels.
+        window_size (tuple[int]): The height and width of the window.
+        num_heads (int): Number of attention heads.
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set
+        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
+        proj_drop (float, optional): Dropout ratio of output. Default: 0.0
+    """
+
+    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
+
+        super().__init__()
+        self.dim = dim
+        self.window_size = window_size  # Wh, Ww
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        # define a parameter table of relative position bias
+        self.relative_position_bias_table = nn.Parameter(
+            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+
+        # get pair-wise relative position index for each token inside the window
+        coords_h = torch.arange(self.window_size[0])
+        coords_w = torch.arange(self.window_size[1])
+        coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
+        coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
+        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
+        relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
+        relative_coords[:, :, 0] += self.window_size[0] - 1  # shift to start from 0
+        relative_coords[:, :, 1] += self.window_size[1] - 1
+        relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
+        relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
+        self.register_buffer("relative_position_index", relative_position_index)
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        trunc_normal_(self.relative_position_bias_table, std=.02)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x, mask=None):
+        """
+        Args:
+            x: input features with shape of (num_windows*B, N, C)
+            mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
+        """
+        B_, N, C = x.shape
+        qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
+
+        q = q * self.scale
+        attn = (q @ k.transpose(-2, -1))
+
+        relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
+            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
+        relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
+        attn = attn + relative_position_bias.unsqueeze(0)
+
+        if mask is not None:
+            nW = mask.shape[0]
+            attn = attn.view(B_ // nW, nW, self.num_heads, N, N) + mask.unsqueeze(1).unsqueeze(0)
+            attn = attn.view(-1, self.num_heads, N, N)
+            attn = self.softmax(attn)
+        else:
+            attn = self.softmax(attn)
+
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class SparseAttnLayer(nn.Module):
+    def __init__(self, inplanes, planes,  norm_layer=nn.BatchNorm2d, islocal=True):
+        super().__init__()
+        self.midplanes = 512 # inplanes//4 if inplanes == 2048 else inplanes
+        self.window_size = 8
+        self.num_heads = self.midplanes//32
+        self.islocal = islocal
+        self.attn = WindowAttention(self.midplanes,  (self.window_size, self.window_size), self.num_heads)
+        # self.H = self.W = None
+        self.mlp = Mlp(in_features=self.midplanes, hidden_features=self.midplanes, act_layer=nn.GELU, drop=0.)
+        self.norm1 = norm_layer(self.midplanes)
+        self.norm2 = norm_layer(self.midplanes)
+        
+        self.drop_path_rate = 0.1
+        self.drop_path = DropPath(self.drop_path_rate) if self.drop_path_rate > 0. else nn.Identity()
+        
+    def forward(self, x) :
+        shortcut = x
+        x = self.norm1(x)
+        B, C, H, W = x.shape
+        x_windows = window_partition(x, self.window_size, islocal=self.islocal)
+        # x_windows = x_windows.view(-1, self.window_size * self.window_size, self.midplanes)
+        
+        attn_windows = self.attn(x_windows)
+        attn_windows = attn_windows.view(-1, self.window_size, self.window_size, self.midplanes)
+        x = window_reverse(attn_windows, self.window_size, H, W, islocal=self.islocal) # B, C, H, W
+        x = shortcut + self.drop_path(x)
+        
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        
+        return x
+    
+class SparseAttnBlock(nn.Module) :
+    def __init__(self, inplanes=512, planes=512,  norm_layer=nn.BatchNorm2d) :
+        super().__init__()
+        self.localattn1 = SparseAttnLayer(inplanes, planes,  norm_layer, islocal=True)
+        self.globalattn1 = SparseAttnLayer(inplanes, planes,  norm_layer, islocal=False)
+        
+        self.localattn2 = SparseAttnLayer(inplanes, planes,  norm_layer, islocal=True)
+        self.globalattn2 = SparseAttnLayer(inplanes, planes,  norm_layer, islocal=False)  
+        
+    def forward(self, x) :
+        x = self.localattn1(x)
+        x = self.globalattn1(x)
+        x = self.localattn2(x)
+        x = self.globalattn2(x)
+        
+        return x
 
 class TwoMLPHead(nn.Module):
     """
--- cls_utils/model.py
+++ cls_utils/model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .block import Bottleneck, TwoMLPHead, RoIPool
+from .block import Bottleneck, TwoMLPHead, RoIPool, SparseAttnBlock
 
 
 class PapsClassificationModel(nn.Module):
@@ -19,32 +19,37 @@ class PapsClassificationModel(nn.Module):
         self.arch = arch
         self.pretrained = pretrained
         self.num_classes = num_classes
+        self.midplanes = 512
         
         self.model = timm.create_model(self.arch, pretrained=self.pretrained, num_classes=0, global_pool='')
         in_features = timm.create_model(self.arch, pretrained=False, num_classes=self.num_classes).get_classifier().in_features 
-        self.interlayer = Bottleneck(in_features, in_features//4)
+        self.conv = nn.Conv2d(in_features, self.midplanes, kernel_size=1, stride=1, bias=False)
+        
+        # use conv (bottleneck from resnet)
+        # self.interlayer = Bottleneck(self.midplanes, self.midplanes)
+        
+        # use local + global attn block
+        self.interlayer = SparseAttnBlock(self.midplanes, self.midplanes)
         
         self.roi_pool = RoIPool((1,1), float(1/32)) # float(1/32), boxes is not normalized
-        
-        intermdeiate_channels = in_features//4
-        self.mlp1 = TwoMLPHead(in_features, intermdeiate_channels, self.num_classes)   
-        self.mlp2 = TwoMLPHead(in_features, intermdeiate_channels, self.num_classes)  
+
+        self.fc1 = nn.Linear(self.midplanes, self.num_classes)   
+        self.fc2 = nn.Linear(self.midplanes, self.num_classes)  
+        self.fc3 = nn.Linear(self.midplanes, self.num_classes)  
         
         self.maxpool = nn.MaxPool1d(2, stride=2)
         
-        for m in self.interlayer.modules():
-            self.init_layer(m)
-                
-        for m in self.mlp1.modules():
-            self.init_layer(m)
+        for m in self.modules():
+            self.init_layer(m)  
             
-        for m in self.mlp2.modules():
-            self.init_layer(m)            
+        # load pretrained model
+        if self.pretrained :
+            pass
                 
     def init_layer(self, m) :
         if isinstance(m, nn.Conv2d):
             nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-        elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+        elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm, nn.LayerNorm)):
             nn.init.constant_(m.weight, 1)
             nn.init.constant_(m.bias, 0)          
                 
@@ -52,15 +57,19 @@ class PapsClassificationModel(nn.Module):
     def forward(self, x, boxes):
         batch_size = x.shape[0]
         x = self.model(x)
+        x = self.conv(x)
         x = self.interlayer(x)   
         
         boxes = [ b.squeeze(dim=0) for b in torch.split(boxes, 1, dim=0)]
         
-        roi = self.roi_pool(x, boxes)
-        output1 = self.mlp1(roi)
-        output2 = self.mlp2(roi)
+        x = self.roi_pool(x, boxes)
+
+        x = x.flatten(start_dim=1)
+        output1 = F.relu(self.fc1(x))
+        output2 = F.relu(self.fc2(x))
+        output3 = F.relu(self.fc3(x))
         
-        outputs = torch.stack([output1, output2], dim=2)
+        outputs = torch.stack([output1, output2, output3], dim=2)
         outputs = self.maxpool(outputs).squeeze(dim=-1)
         
         return outputs
